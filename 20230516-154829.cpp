f94f33eeaa8ad162b4842e92a5eda4cfe08f044e
unknown <you@example.com>
2024-05-12 15:56:23 -0700

    hello

---
+++ 20230516-154829.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+int st;
+
+int name;
+int age= 22;
+float salary =100.23;
+
+
+cout <<"please enter the name";
+cout <<"the result name is="<<"chaltu"<<endl;
+cout<<"please enter the age ";
+cout <<"the result age is ="<<"22"<<endl;
+cout <<"please enter the salary";
+cout <<"the result salary is="<<"100.23"<<endl;
+
+return 0;
+
+   }
+    
+   
